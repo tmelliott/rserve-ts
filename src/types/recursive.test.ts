c491cdf7da9628a374619d45dc0a53d75d02dda8
Tom Elliott <tomelliottnz@gmail.com>
2025-05-08 11:53:22 +1200

    remove optionality; and helper function to refine

--- src/types/recursive.test.ts
+++ src/types/recursive.test.ts
@@ -1,31 +1,36 @@
 import { test, expect } from "vitest";
 import { z } from "zod";
 import _recursive_list from "./recursive";
-import Robj from ".";
+import { Unify, UnifyOne } from "./helpers";
 
 test("Recursive list with simple nesting", () => {
   const baseRecursiveListSchema = z.object({
     label: z.string(),
-    heading: z.string().optional(),
     r_type: z.literal("vector"),
     r_attributes: z.object({
       names: z.string().or(z.string().array()),
     }),
   });
 
-  type RecursiveList = z.infer<typeof baseRecursiveListSchema> & {
-    sublist?: RecursiveList;
-  };
+  type RecursiveList =
+    | (z.infer<typeof baseRecursiveListSchema> & {
+        sublist: RecursiveList;
+      })
+    | (z.infer<typeof baseRecursiveListSchema> & {
+        heading: string;
+      });
 
-  const validList = (x: RecursiveList): boolean => {
-    if (x.heading) return true;
-    if (!x.sublist) return false;
-    return validList(x.sublist);
+  const validList = (x: {}): boolean => {
+    if ("heading" in x && typeof x.heading === "string") return true;
+    if ("sublist" in x && typeof x.sublist === "object" && x.sublist)
+      return validList(x.sublist);
+    return false;
   };
   const recursiveListSchema = _recursive_list<RecursiveList>(
     baseRecursiveListSchema,
     (self) => ({
       sublist: self.optional(),
+      heading: z.string().optional(),
     })
   ).refine(validList, {
     message: "One of heading or sublist must be specified",
