4d061fc24d988faeede1d437db04e9748468b494
Tom Elliott <tomelliottnz@gmail.com>
2025-10-13 19:50:49 +1300

    callback helper

--- src/helpers.ts
+++ src/helpers.ts
@@ -35,3 +35,16 @@ export function isRServeError(x: unknown): x is RServeError {
     typeof x[1] === "number"
   );
 }
+
+export function cb<A extends any[], R>(fn: (...args: A) => Promise<R> | R) {
+  return function (
+    ...args: [...A, callback: (err: any, data?: R) => void]
+  ): void {
+    const c = args.pop() as (err: any, data?: R) => void;
+    const a: A = args as any;
+
+    Promise.resolve(fn(...a))
+      .then((data) => c(null, data))
+      .catch((err) => c(err));
+  };
+}
